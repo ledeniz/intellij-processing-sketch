7c10d2178241f89e600650a98de05d6937d60dcf
Deniz Erdogan <hello@ledeniz.de>
2019-06-22 16:03:07 +0200

    initial commit

---
+++ src/Processing.java
@@ -0,0 +1,22 @@
+import processing.core.*;
+
+public class Processing extends PApplet {
+	public int width  = 640;
+	public int height = 640;
+	
+	public static void main(String[] args) {
+		PApplet.main(Sketch.class);
+	}
+	
+	public void settings() {
+		size(this.width, this.height);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+}
---
+++ src/Sketch.java
@@ -0,0 +1,3 @@
+public class Sketch extends Processing {
+	
+}
